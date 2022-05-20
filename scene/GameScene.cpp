d693c5f7b77b40554ecf19f838c8eb7076e57ffd
KensyuuEn <k021g1321@g.neec.ac.jp>
2022-05-20 13:39:56 +0900

    re02_02

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -43,6 +43,95 @@ void GameScene::Initialize() {
 
 	// ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
 	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+
+	// X,Y,Z方向のスケーリングを設定
+	worldTransform_.scale_ = { 5,1,1 };
+
+	// スケーリング行列を宣言
+	Matrix4 matScale;
+
+	matScale.m[0][0] = worldTransform_.scale_.x;
+	matScale.m[1][1] = worldTransform_.scale_.y;
+	matScale.m[2][2] = worldTransform_.scale_.z;
+	matScale.m[3][3] = 1;
+
+	//  単位行列を宣言
+	Matrix4 matIdentity;
+
+	matIdentity.m[0][0] = 1;
+	matIdentity.m[1][1] = 1;
+	matIdentity.m[2][2] = 1;
+	matIdentity.m[3][3] = 1;
+
+	float pi = 3.14f;
+	// X,Y,Z軸周りの回転角を設定
+	worldTransform_.rotation_ = { pi / 4,pi / 4,pi / 4 };
+
+	// X,Y,Z軸回転行列を宣言
+	Matrix4 matRotX, matRotY, matRotZ;
+
+	// 合成用回転行列を宣言
+	Matrix4 matRot = matIdentity;
+
+	// Z軸回転行列
+	matRotZ.m[0][0] = cos(worldTransform_.rotation_.z);
+	matRotZ.m[0][1] = sin(worldTransform_.rotation_.z);
+	matRotZ.m[1][0] = -sin(worldTransform_.rotation_.z);
+	matRotZ.m[1][1] = cos(worldTransform_.rotation_.z);
+	matRotZ.m[2][2] = 1;
+	matRotZ.m[3][3] = 1;
+
+	// X軸回転行列
+	matRotX.m[0][0] = 1;
+	matRotX.m[1][1] = cos(worldTransform_.rotation_.x);
+	matRotX.m[1][2] = sin(worldTransform_.rotation_.x);
+	matRotX.m[2][1] = -sin(worldTransform_.rotation_.x);
+	matRotX.m[2][2] = cos(worldTransform_.rotation_.x);
+	matRotX.m[3][3] = 1;
+
+	// Y軸回転行列
+	matRotY.m[0][0] = cos(worldTransform_.rotation_.y);
+	matRotY.m[0][2] = -sin(worldTransform_.rotation_.y);
+	matRotY.m[1][1] = 1;
+	matRotY.m[2][0] = sin(worldTransform_.rotation_.y);
+	matRotY.m[2][2] = cos(worldTransform_.rotation_.y);
+	matRotY.m[3][3] = 1;
+
+	// 各軸の回転行列を合成
+	matRot *= matRotZ;
+	matRot *= matRotX;
+	matRot *= matRotY;
+
+	// X,Y,Z軸周りの平行移動を設定
+	worldTransform_.translation_ = { 10.0f,10.0f,10.0f };
+
+	// 平行移動行列を宣言
+	Matrix4 matTrans = MathUtility::Matrix4Identity();
+
+	// 平行移動行列
+	matTrans.m[0][0] = 1;
+	matTrans.m[1][1] = 1;
+	matTrans.m[2][2] = 1;
+	matTrans.m[3][0] = 10;
+	matTrans.m[3][1] = 10;
+	matTrans.m[3][2] = 10;
+	matTrans.m[3][3] = 1;
+
+	worldTransform_.matWorld_ = matIdentity;
+
+	worldTransform_.matWorld_ *= matScale;
+	
+	//worldTransform_.matWorld_ *= matRotZ;
+	//worldTransform_.matWorld_ *= matRotX;
+	//worldTransform_.matWorld_ *= matRotY;
+	
+	worldTransform_.matWorld_ *= matRot;
+
+	worldTransform_.matWorld_ *= matTrans;
+
+	// 行列の転送
+	worldTransform_.TransferMatrix();
+
 }
 
 void GameScene::Update() {
@@ -76,10 +165,10 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	//model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 
 	// ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
-	PrimitiveDrawer::GetInstance()->DrawLine3d({ 0,0,0 }, { 70,-70,0 }, { 1,0,0,1 });
+	//PrimitiveDrawer::GetInstance()->DrawLine3d({ 0,0,0 }, { 70,-70,0 }, { 1,0,0,1 });
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
